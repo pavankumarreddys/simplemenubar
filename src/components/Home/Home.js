992f801285c0a2d5539e07da24bd4cd5b93115d9
Pavan Kumar Reddy Sannala <pavanreddyr34@gmail.com>
2023-10-28 22:45:05 +0530

    si

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,49 @@ import React from 'react'
 import { Header } from '../Header/Header'
 export const Home = () => {
   return (
-    <div>
+    <div className='home-container'>
         <Header/>
+        <div className='container'>
+          <div className='row'>
+
+            <div className='col-12 col-md-6 p-3'>
+              <div className='white-card mb-3'>
+                hii
+              </div>
+            </div>
+
+            <div className='col-12 col-md-6 p-3'>
+              <div className='white-card mb-3'>
+                hi
+              </div>
+            </div>
+
+            <div className='col-12 col-md-6 p-3'>
+              <div className='white-card mb-3'>
+                hi
+              </div>
+            </div>
+
+            <div className='col-12 col-md-6 p-3'>
+              <div className='white-card mb-3'>
+                hi
+              </div>
+            </div>
+
+            <div className='col-12 col-md-6 p-3'>
+              <div className='white-card mb-3'>
+                hi
+              </div>
+            </div>
+
+            <div className='col-12 col-md-6 p-3'>
+              <div className='white-card mb-3'>
+                hi
+              </div>
+            </div>
+
+          </div>
+        </div>
     </div>
   )
 }
